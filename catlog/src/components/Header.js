8a4ffd352abba940511291f1907d6558cafb1d13
tharunkesana8978 <tharunkesana8978@gmail.com>
2024-12-10 10:25:53 +0530

    header&card

---
+++ catlog/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1>E-Commerce Product Catalog</h1>
+  </header>
+);
+
+export default Header;
