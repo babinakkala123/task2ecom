15c426607705223f0300ca6222401994af3d4c6c
babinakkala123 <nakkalababi123@gmail.com>
2024-12-10 13:10:38 +0530

    modified app.js in app.listen

--- backend/app.js
+++ backend/app.js
@@ -87,4 +87,4 @@ app.delete('/api/products/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(`PORT, () => console.log(Server running on port ${PORT})`);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
